847fa1cc86cd0b0e9bc1619630ca92110dbea757
remembrance <1103314553@qq.com>
2021-09-21 19:20:24 +0800

    second commit

--- SpringPractice/src/main/java/com/rem/aop/Main.java
+++ SpringPractice/src/main/java/com/rem/aop/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         MyCalcImpl myCalc = new MyCalcImpl();
         //myCalc calc = CalcProxy.getInstance(myCalc)
+        System.out.println("hello world");
     }
 }
